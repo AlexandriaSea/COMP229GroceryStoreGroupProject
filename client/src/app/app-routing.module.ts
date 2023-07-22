d8f69ecdfad8c75b63c4ff967cb0b288eaa61f03
antonyyfk <109820108+antonyyfk@users.noreply.github.com>
2023-07-22 19:28:31 -0400

    Update welcome page routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 
-const routes: Routes = [{ path: '', redirectTo: 'products', pathMatch: 'full' },
+const routes: Routes = [{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
+{ path: 'welcome', component: WelcomePageComponent },
 { path: 'products', component: ProductsListComponent },
 { path: 'products/:id', component: ProductDetailsComponent },
 { path: 'add', component: AddProductComponent }];
