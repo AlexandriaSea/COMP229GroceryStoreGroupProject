dcff32a7222026a06d3079393cb9b700cf997e79
Jingya Huang <jhuan180@my.centennialcollege.ca>
2023-07-26 18:22:20 -0400

    created authentication serveices and components including register, login, profile; created role-based(public and protected) components; configured App Routing Module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,25 @@ import { WelcomePageComponent } from './components/welcome-page/welcome-page.com
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+    
+{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 { path: 'welcome', component: WelcomePageComponent },
 { path: 'products', component: ProductsListComponent },
 { path: 'products/:id', component: ProductDetailsComponent },
