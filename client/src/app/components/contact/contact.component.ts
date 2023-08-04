5ec1b36d2f3c67e32f7f04cbb9f81059c3d514f5
antonyyfk <109820108+antonyyfk@users.noreply.github.com>
2023-08-04 14:25:09 -0400

    latest

--- client/src/app/components/contact/contact.component.ts
+++ client/src/app/components/contact/contact.component.ts
@@ -1,7 +1,7 @@
-
-
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Contact } from 'src/app/models/contact.model';
+import { StorageService } from 'src/app/_services/storage.service';
 import { ContactService } from 'src/app/services/contact.service';
 @Component({
     selector: 'app-contact',
@@ -13,11 +13,65 @@ export class ContactComponent implements OnInit {
     currentContact: Contact = {};
     currentIndex = -1;
     name = '';
+    contact: Contact = {
+        first_name: '',
+        last_name: '',
+        contact_number: '',
+        email_address: '',
+        message: '',
+    };
+    submitted = false;
 
-    constructor(private contactService: ContactService) { }
+    private roles: string[] = [];
+    isLoggedIn = false;
+    showAdminBoard = false;
+    showModeratorBoard = false;
+    username?: string;
+
+    constructor(private contactService: ContactService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private storageService: StorageService) { }
     ngOnInit(): void {
         this.retrieveContacts();
+        this.isLoggedIn = this.storageService.isLoggedIn();
+
+        if (this.isLoggedIn) {
+          const user = this.storageService.getUser();
+          this.roles = user.roles;
+          this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+          this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+          //this.username = user.username;
+        }
     }
+    saveContact(): void {
+        if (!this.contact.first_name ||
+            !this.contact.last_name ||
+            !this.contact.contact_number ||
+            !this.contact.email_address ||
+            !this.contact.message) {
+            alert("All five fields are mandatory!");
+            return;
+        }
+        const data = {
+            first_name: this.contact.first_name,
+            last_name: this.contact.last_name,
+            contact_number: this.contact.contact_number,
+            email_address: this.contact.email_address,
+            message: this.contact.message
+        };
+
+        this.contactService.create(data)
+            .subscribe({
+                next: (res) => {
+                    console.log(res);
+                    alert("The message was sent successfully!");
+
+                },
+                error: (e) => console.error(e)
+            });
+    }
+
     retrieveContacts(): void {
         this.contactService.getAll()
             .subscribe({
@@ -60,9 +114,9 @@ export class ContactComponent implements OnInit {
             });
     }
 
-    onSubmit()
-    {
-        alert("The message was sent successfully!");
+    onSubmit(): void {
+        this.saveContact()
+
     }
 }
 
